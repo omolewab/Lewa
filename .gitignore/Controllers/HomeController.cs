95d226b7883f41770b27161db896484dc05f448e
Blessing Okeoghene Omobo <37213581+omolewab@users.noreply.github.com>
2018-05-17 16:22:00 +0100

    Add files via upload

---
+++ .gitignore/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lewa.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This page describes who we are, what we do and what we are about. So enjoy!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Get in touch with us, we love you.";
+
+            return View();
+        }
+    }
+}
